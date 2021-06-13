2b5bf37d545701690e8e1665f21dc380a20596ac
chriamue <7541795+chriamue@users.noreply.github.com>
2021-06-13 10:40:15 +0000

    Deploying to gh-pages from @ lenna-project/lenna-cli@c65d9e7a9618bc68f26616ca754b5d2bb19cc06a 🚀

--- main.js
+++ main.js
@@ -312,7 +312,7 @@ const save = async (image, format) => {
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"527":{"977":"420ae51bbe4e92d96de9"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"527":{"977":"9291b54c51c55c5dc2e5"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
